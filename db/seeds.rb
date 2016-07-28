281ece49b0de5f83dfe7ce8e32979a45c3aa127c
kamitetsuyuki <kaameeng@gmail.com>
2016-07-28 01:27:38 +0000

    syuusei1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,28 @@
+
+puts "seeds runninng..."
+
+
+# マイクロポスト
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+  print "."
+end
+puts "\n-> Micropost_seeds OK!!"
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed); print "." }
+puts "\n-> Following_seeds OK!!"
+followers.each { |follower| follower.follow(user); print "." }
+puts "\n-> Follower_seeds OK!!"
+
+puts "\n=> All_seeds OK!!"
+>>>>>>>>> local version
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
