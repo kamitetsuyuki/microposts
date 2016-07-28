953539bb52cc30254bc90da86cd1f6ebff7a6570
kamitetsuyuki <kaameeng@gmail.com>
2016-07-28 09:48:17 +0000

    merged2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,22 @@
-
 puts "seeds runninng..."
 
+# ユーザー
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+print "."
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+              email: email,
+              password:              password,
+              password_confirmation: password)
+  print "."
+end
+puts "\n-> User_seeds OK!!"
 
 # マイクロポスト
 users = User.order(:created_at).take(6)
@@ -22,10 +38,3 @@ followers.each { |follower| follower.follow(user); print "." }
 puts "\n-> Follower_seeds OK!!"
 
 puts "\n=> All_seeds OK!!"
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
